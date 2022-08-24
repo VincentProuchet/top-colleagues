29bc3639c3b30ee91e4a23f5d689b8e4d9942d7c
Vincent Prouchet <prouchetvincent@hotmail.com>
2022-08-24 16:24:38 +0200

    Composant Collegues
    ajout d'un composant
    et affichage dans le composant principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ColleguesComponent } from './collegues/collegues.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColleguesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/collegues/collegues.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tc-collegues',
+  templateUrl: './collegues.component.html',
+  styleUrls: ['./collegues.component.scss']
+})
+export class ColleguesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

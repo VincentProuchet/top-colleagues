cae737adc3712b83343ee8352bc9f5aa2f91bc18
Vincent Prouchet <prouchetvincent@hotmail.com>
2022-08-24 17:25:57 +0200

    Mise à jour COmposant Collegues
    Création assisté d'un template
    avec des variables

--- src/app/collegues/collegues.component.ts
+++ src/app/collegues/collegues.component.ts
@@ -2,10 +2,27 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'tc-collegues',
-  templateUrl: './collegues.component.html',
+  template: `
+    <h1>Ici un template</h1>
+
+    <!-- [] => biding de propriété  -->
+    <img [src]="uneImage" alt="Une Image par binding">
+    <!--  *=> directive de structure d'angular -->
+    <p **ngIf="unChamp">
+      <!--{{}}=> interpolation : affichage d'une variable, du retour d'une fonction ... -->
+      {{unChamp}}
+      <!-- Pipe  => permet de transformer l'affichage de la donnée -->
+      {{unChamp|uppercase}}
+    </p>
+
+  `,
+  // templateUrl: './collegues.component.html',
   styleUrls: ['./collegues.component.scss']
 })
 export class ColleguesComponent implements OnInit {
+
+  uneImage = "/assets/logo.jpg";
+  unChamp = "Je suis un champ ts"
 
   constructor() { }
 

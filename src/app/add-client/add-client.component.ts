3246b87ebf431ef1d4849442d65d555584e70594
Vincent Prouchet <prouchetvincent@hotmail.com>
2022-08-25 15:37:08 +0200

    utilisation d'une classe en ANGULAR

--- src/app/add-client/add-client.component.ts
+++ src/app/add-client/add-client.component.ts
@@ -7,31 +7,48 @@ import { Personne } from '../model/personne';
   styleUrls: ['./add-client.component.scss']
 })
 export class AddClientComponent implements OnInit {
-  nom !: string;
-  prenom!: string;
-  adresse !: string;
+  /**
+   *
+   nom !: string;
+   prenom!: string;
+   adresse !: string;
+   */
+  personne!: Personne;
   tabPersonnes: Array<Personne> = new Array();
-
+  static ERROR_NEW_CLIENT_TEXT: string = "les valeurs du tableau sont vides ou incohérentes";
 
 
   constructor() {
   }
 
   ngOnInit(): void {
-    this.nom = "";
-    this.prenom = "";
-    this.adresse = "";
+    this.personne = new Personne("", "", "");
+
   }
 
   ajouter(): void {
-    if (this.testValue(this.nom)
-      && this.testValue(this.prenom)
-      && this.testValue(this.adresse)) {
-      this.tabPersonnes.push(new Personne(this.nom, this.prenom, this.adresse));
-      console.log(this.tabPersonnes);
+    if (this.testValue(this.personne.nom)
+      && this.testValue(this.personne.prenom)
+      && this.testValue(this.personne.adresse)) {
+      // va créer un probléme de références
+      // tous les élements du tableau seraient des clone du même objet
+      //this.tabPersonnes.push(this.personne);
+      // celui-ci pousse une copie de l'objet courant
+      // au lieu de passer la référence l'objet
+      this.tabPersonnes.push({ ...this.personne });
+      // évite de devoir faire ça
+      /**
+       * à la vieille école
+       this.tabPersonnes.push(new Personne(
+         this.personne.nom
+         , this.personne.prenom
+         , this.personne.adresse));
 
+         */
+
+      //console.log(this.tabPersonnes);
     }
-    console.log("les valeurs du tableau sont vides ou incohérentes");
+    console.log(AddClientComponent.ERROR_NEW_CLIENT_TEXT);
 
   }
   testValue(value: string): boolean {
